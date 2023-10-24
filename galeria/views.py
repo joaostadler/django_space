662fc27bd5fbc4498b429fcf2911d586a97a5604
joaostadler <joaostadler@gmail.com>
2023-10-23 22:45:11 -0300

    django_space

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+        1: {"nome" : "Nebulosa de Carina",
+            "legenda" : "webbtelecope.org / NASA / James Webb"},
+        2: {
+            "nome": "Galáxia NGC",
+            "legenda" : "nasa.org"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards" : dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
